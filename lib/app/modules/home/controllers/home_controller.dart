4e976423a2f9cec90598d04c37e878c3533ab7b5
emadbeltaje <emadbeltaje@gmail.com>
2022-10-18 16:53:23 +0300

    update api call example ⭐️

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -5,35 +5,37 @@ import '../../../services/api_call_status.dart';
 import '../../../services/base_client.dart';
 
 class HomeController extends GetxController {
-  // hold data
+  // hold data coming from api
   List<dynamic>? data;
   // api call status
   ApiCallStatus apiCallStatus = ApiCallStatus.holding;
 
-  // getting data from api simulating
+  // getting data from api
   getData() async {
-    // *) indicate loading state
-    apiCallStatus = ApiCallStatus.loading;
-    update();
     // *) perform api call
     await BaseClient.safeApiCall(
       Constants.todosApiUrl, // url
       RequestType.get, // request type (get,post,delete,put)
+      onLoading: () {
+        // *) indicate loading state
+        apiCallStatus = ApiCallStatus.loading;
+        update();
+      },
       onSuccess: (response){ // api done successfully
         data = List.from(response.data);
-        // -) indicate success state
+        // *) indicate success state
         apiCallStatus = ApiCallStatus.success;
         update();
       },
       // if you don't pass this method base client
-      // will automaticly handle error and show message
+      // will automaticly handle error and show message to user
       onError: (error){
         // show error message to user
         BaseClient.handleApiError(error);
-        // -) indicate error status
+        // *) indicate error status
         apiCallStatus = ApiCallStatus.error;
         update();
-      }, // error while performing request
+      },
     );
   }
 
