8aacd1edc2ec543e0511ebd90dab9f9129a1a397
emadbeltaje <emadbeltaje@gmail.com>
2022-10-25 21:22:15 +0300

    fix ios module not found ⛑

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,7 +1,6 @@
 import UIKit
 import Flutter
 import awesome_notifications
-import shared_preferences_ios
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -15,8 +14,6 @@ import shared_preferences_ios
         SwiftAwesomeNotificationsPlugin.setPluginRegistrantCallback { registry in
             SwiftAwesomeNotificationsPlugin.register(
               with: registry.registrar(forPlugin: "io.flutter.plugins.awesomenotifications.AwesomeNotificationsPlugin")!)
-            FLTSharedPreferencesPlugin.register(
-              with: registry.registrar(forPlugin: "io.flutter.plugins.sharedpreferences.SharedPreferencesPlugin")!)
         }
 
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
