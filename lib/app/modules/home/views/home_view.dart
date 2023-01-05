0edaaa5ee00321a47a75574ef6bb4163e07af7b1
emadbeltaje <emadbeltaje@gmail.com>
2023-01-05 11:54:53 +0200

    Refactor Code 🛠️

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -6,9 +6,10 @@ import '../../../components/my_widgets_animator.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
+  const HomeView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    var theme = Theme.of(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text('HomeView'),
@@ -17,22 +18,21 @@ class HomeView extends GetView<HomeController> {
       body: GetBuilder<HomeController>(
         builder: (_){
           return MyWidgetsAnimator(
-              apiCallStatus: controller.apiCallStatus,
-              loadingWidget: () => const Center(child: CircularProgressIndicator(),),
-              errorWidget: ()=> const Center(child: Text('Something went worng!'),),
-              successWidget: () =>
-                 ListView.separated(
-                  itemCount: controller.data!.length,
+            apiCallStatus: controller.apiCallStatus,
+            loadingWidget: () => const Center(child: CircularProgressIndicator(),),
+            errorWidget: ()=> const Center(child: Text('Something went Wrong!'),),
+            successWidget: () =>
+                ListView.separated(
+                  itemCount: controller.data?.length ?? 0,
                   separatorBuilder: (_,__) => SizedBox(height: 10.h,),
                   itemBuilder: (ctx,index) => ListTile(
-                      title: Text(controller.data![index]['userId'].toString()),
-                      subtitle: Text(controller.data![index]['title']),
-                    ),
+                    title: Text(controller.data![index]['userId'].toString()),
+                    subtitle: Text(controller.data![index]['title']),
+                  ),
                 ),
-
           );
         },
       ),
-      );
+    );
   }
 }
